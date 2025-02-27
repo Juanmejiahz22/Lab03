675ed817bf392eaafdbe9052b14d4ddc908f1e55
Juan José Mejía Celis <142859767+Juanmejiahz22@users.noreply.github.com>
2025-02-26 21:07:46 -0500

    Add files via upload

---
+++ Library/src/main/java/edu/eci/cvds/tdd/library/loan/Loan.java
@@ -0,0 +1,71 @@
+package edu.eci.cvds.tdd.library.loan;
+
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.user.User;
+import java.util.Objects;
+import java.time.LocalDateTime;
+
+public class Loan {
+    private Book book;
+    private User user;
+    private LocalDateTime loanDate;
+    private LoanStatus status;
+    private LocalDateTime returnDate;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDateTime loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LoanStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(LoanStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDateTime returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Loan loan = (Loan) o;
+        return Objects.equals(book, loan.book) &&
+               Objects.equals(user, loan.user) &&
+               Objects.equals(loanDate, loan.loanDate) &&
+               status == loan.status &&
+               Objects.equals(returnDate, loan.returnDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(book, user, loanDate, status, returnDate);
+    }
+}
